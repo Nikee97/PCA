b0cfc365898556f6ca3b9b5eeeada8814b2533f0
Nikita patel <41582500+Nikee97@users.noreply.github.com>
2020-02-04 18:10:28 +0530

    Update PCA task.py

--- PCA task.py
+++ PCA task.py
@@ -1,114 +1,63 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# # importing the libraries
-
-# In[5]:
-
-
+# importing the libraries
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
-
-# # importing the dataset
-
-# In[6]:
-
-
-#i have used one of the famous wine dataset for this task.
+# importing the dataset - i have used one of the famous wine dataset for this task.
 dataset = pd.read_csv('Wine.csv')
 dataset.head()
-
-
-# In[7]:
-
-
 X = dataset.iloc[:,0:13].values
 y = dataset.iloc[:,13].values
 
 
-# # splitting the dataset into the training dataset and test set
-
-# In[8]:
-
-
+# splitting the dataset into the training dataset and test set
 from sklearn.model_selection import train_test_split
 X_train,X_test,y_train,y_test = train_test_split(X,y,test_size = 0.2,random_state = 0)
 
 
-# 
-# # feature scaling
-
-# In[9]:
-
-
+# feature scaling
 from sklearn.preprocessing import StandardScaler
 sc = StandardScaler()
 X_train = sc.fit_transform(X_train)
 X_test = sc.transform(X_test)
 
 
-# # applying PCA
-
-# In[10]:
-
-
+# applying PCA
 from sklearn.decomposition import PCA
 pca = PCA(n_components=2)
 pca.fit(X)
-
 X_train = pca.fit_transform(X_train)
 X_test = pca.transform(X_test)
 explained_variance = pca.explained_variance_ratio_
-
 print(X_test)
 print(explained_variance)
 
 
-# # fitting logistic regression to the training set
-
-# In[11]:
-
-
+# fitting logistic regression to the training set
 from sklearn.linear_model import LogisticRegression
 classifier = LogisticRegression(random_state = 0)
 classifier.fit(X_train,y_train)
 
 
-# # predicting the test set result
-
-# In[12]:
-
-
+# predicting the test set result
 y_pred = classifier.predict(X_test)
 
 
-# # making confusion matrix
-
-# In[13]:
-
-
+# making confusion matrix
 from sklearn.metrics import confusion_matrix
 cm = confusion_matrix(y_test,y_pred)
 
 
-# In[14]:
-
-
 # Visualising the Training set results
-
-
 from matplotlib.colors import ListedColormap
 X_set, y_set = X_train, y_train
-
 X1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min() - 1, stop = X_set[:, 0].max() + 1, step = 0.01),
                      np.arange(start = X_set[:, 1].min() - 1, stop = X_set[:, 1].max() + 1, step = 0.01))
-
 plt.contourf(X1, X2, classifier.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape),
              alpha = 0.75, cmap = ListedColormap(('red', 'green', 'blue')))
-
-
 plt.xlim(X1.min(), X1.max())
 plt.ylim(X2.min(), X2.max())
 for i, j in enumerate(np.unique(y_set)):
@@ -121,14 +70,7 @@ plt.legend()
 plt.show()
                 
                     
-
-
-# In[15]:
-
-
 # Visualising the Test set results
-
-
 from matplotlib.colors import ListedColormap
 X_set, y_set = X_test, y_test
 X1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min() - 1, stop = X_set[:, 0].max() + 1, step = 0.01),
@@ -146,8 +88,6 @@ plt.ylabel('PC2')
 plt.legend()
 plt.show()
 
-
-# In[ ]:
 
 
 
